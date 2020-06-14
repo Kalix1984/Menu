5851cde775e5b8146fd2068425aaa276e53bddbe
Kalix1984 <istvn.horvath@yahoo.com>
2020-06-14 20:10:28 +0200

    first commit

---
+++ Menu/src/application/MenuController.java
@@ -0,0 +1,70 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MenuController {
+	@FXML
+	private Button f1;
+	
+	@FXML
+	private Button f2;
+	
+	@FXML
+	private Button f3;
+	
+	@FXML
+	private Button f4;
+	
+	@FXML
+	private Button exit;
+	
+	@FXML
+	private Pane startpane;
+	
+	@FXML
+	private Pane pane1;
+	
+	@FXML
+	private Pane pane2;
+	
+	@FXML
+	private Pane pane3;
+	
+
+	public void exit(ActionEvent event) {
+		System.out.println("Exit");
+	}
+	
+	public void changeToPane1(ActionEvent event) {
+		System.out.println("pane1");
+		startpane.setVisible(false);
+		pane1.setVisible(true);
+		pane2.setVisible(false);
+		pane3.setVisible(false);
+		
+		
+	}
+	
+	public void changeToPane2(ActionEvent event) {
+		System.out.println("pane2");
+		startpane.setVisible(false);
+		pane1.setVisible(false);
+		pane2.setVisible(true);
+		pane3.setVisible(false);
+	}
+	
+	public void changeToPane3(ActionEvent event) {
+		System.out.println("pane3");
+		startpane.setVisible(false);
+		pane1.setVisible(false);
+		pane2.setVisible(false);
+		pane3.setVisible(true);
+	}
+	
+
+	
+}
